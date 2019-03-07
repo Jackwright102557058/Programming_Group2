1bb816034ef624edaa4d66636d3eaf71dca00b52
Jackwright102557058 <48079170+Jackwright102557058@users.noreply.github.com>
2019-03-07 17:45:01 +1100

    Initial Commit
    sadv

---
+++ calculator.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: 102557058
+ * Date: 7/03/2019
+ * Time: 5:03 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Hello_World
+{
+	class Program {
+		
+		public static void Main(string[] args) {
+			//declarations
+			string message;
+			string name;
+			int num1;
+			int num2;
+			string inputStorage1;
+			string inputStorage2;
+			
+			
+			//initalisations
+     		//message = "Hello";
+			//name = Console.ReadLine();
+			//num1 = 10;
+			//num2 = 20;
+
+			inputStorage1 = Console.ReadLine();
+			inputStorage2 = Console.ReadLine();
+			
+			num1 = int.Parse(inputStorage1);
+			num2 = int.Parse(inputStorage2);
+			
+			int sum = num1 + num2;
+			
+			Console.WriteLine(sum.ToString());
+			
+			//Console.WriteLine(message + " " + name);
+			//Console.WriteLine(num1 + num2);
+					
+			Console.Write("Press any key to continue? ");
+			Console.ReadKey(true);
+		}
+	}
+}
