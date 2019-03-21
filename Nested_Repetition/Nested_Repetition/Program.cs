c570abe6020c029ac8b4937eea6fc079fa7d72b3
Jackwright102557058 <48079170+Jackwright102557058@users.noreply.github.com>
2019-03-21 17:19:06 +1100

    Nested_Repititon
    Task 1 Part 1 and 2

---
+++ Nested_Repetition/Nested_Repetition/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: 102557058
+ * Date: 21/03/2019
+ * Time: 2:56 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Nested_Repetition
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.Write("Please Enter your Name ");
+			String name = Console.ReadLine();
+			
+			int counter = 0;
+			int counter2 = 0;
+			
+			while (counter < 10) {
+				counter2 = 0;
+				
+				while (counter2 < 10) {
+					Console.Write(name);
+					counter2 += 1;
+				}
+				
+				Console.WriteLine();
+				
+				counter += 1;
+					
+			}
+			Console.WriteLine("Later Nerd");
+			Console.ReadKey();
+		}
+	}
+}
