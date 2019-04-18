d6764f28f654bbd6f9808824a789af1f2d298a0c
JACK WRIGHT <102557058@student.swin.edu.au>
2019-04-18 15:23:48 +1000

    nested 1 and 2

---
+++ nested_repetition1_2.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: 102557058
+ * Date: 18/04/2019
+ * Time: 2:17 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;   
+namespace Nested2  
+{  
+   class Program  
+   {  
+      static void Main(string[] args)  
+      {  
+      Console.WriteLine("How many x's do you want?");
+      string uinput = Console.ReadLine();
+      
+      int vinput = int.Parse(uinput);
+    
+       	
+      int count = 0;
+      int Value = 0;
+      while(count < vinput) {
+      	string x = "";
+      	int Count2 = vinput;
+      	while(Count2 > Value){
+      		x = x + "x";
+      		Count2 = Count2 -1;
+      	}
+      	Console.WriteLine(x);
+      	Value = Value +1;
+      	count++;}
+      	
+      	Console.ReadKey();
+      	
+      }
+      }  
+ }
