525a905aca5f22cf4444f748747d52c99b51f25a
Jackwright102557058 <48079170+Jackwright102557058@users.noreply.github.com>
2019-06-20 12:22:52 +1000

    Practice

---
+++ Practice Exercise/Program.cs
@@ -0,0 +1,12 @@
+using system;
+using system.drawing;
+
+namespace PracticeExercise {
+    class program {
+        static void Main (string[] args) {
+
+        }
+             
+
+    }
+}
---
+++ Practice Exercise/Ute.cs
@@ -0,0 +1,5 @@
+        class Ute : Car {
+
+            public Ute (string make, string model, Color colour,  int Wheels, string Rego, int CarryLimit, int Load):base(make, model, colour, Wheels, Rego)
+
+        }
